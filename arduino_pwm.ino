45515a98e02b023fc355ac4850ef90d068f8c364
moritzmoser <mosi.moser@aon.at>
2015-01-29 14:39:47 +0100

    Tagesprotokoll 15-01-29

--- arduino_pwm.ino
+++ arduino_pwm.ino
@@ -1,24 +1,90 @@
-#define rightmotor 5 //Der Pin Nummer 5 wird als S5 definiert. Zu beachten ist, dass es Pin 5 oder 6 sein muss, da nur diese PWM-Signale ausgeben können.
-#define leftmotor 6
-int sensor1 = A0;
-int sensor2 = A1;
+#define right 5
+#define left 6
+#define s1 A1
+#define s2 A2
+#define s3 A3
+#define s4 A4
+
 int drehzahl1 = 0;
 int drehzahl2 = 0;
-long lastprint;
+int arr1 [4];
+int arr2 [4];
+int arr3 [4];
+int arr4 [4];
+
+int x1 = 0;
+int x2 = 0;
+int x3 = 0;
+int x4 = 0;
+
+//long lastprint;
 
 void setup() //S5 &S6 wird als OUTPUT definiert und auf LOW gesetzt.
 {
-  pinMode(leftmotor, OUTPUT);
-  pinMode(leftmotor, LOW);
-  pinMode(rightmotor, OUTPUT);
-  pinMode(rightmotor, LOW);
-  Serial.begin(9600);
-  lastprint = millis();
+  pinMode(left, OUTPUT);
+  pinMode(left, LOW);
+  pinMode(right, OUTPUT);
+  pinMode(right, LOW);
+  //Serial.begin(9600);
+  //lastprint = millis();
+  startroutine();
+}
+
+
+void startroutine()
+{
+  for (int i=0; i<5; i++)
+  {
+    int se1 = analogRead(s1);
+    arr1 [i] = se1;
+    x1 = x1 + se1;
+    int se2 = analogRead(s2);
+    arr2 [i] = se2;
+    x2 = x1 + se2;
+    int se3 = analogRead(s3);
+    arr3 [i] = se3;
+    x3 = x1 + se3;
+    int se4 = analogRead(s4);
+    arr4 [i] = se4;
+    x4 = x1 + se4;
+  }
+  x1 = x1/5;
+  x2 = x2/5;
+  x3 = x3/5;
+  x4 = x4/5;
+}
+
+int berechneX(int mat [])
+{
+  int x = 0;
+  for (int i = 0; i < 5; i++)
+  {
+    x = x + mat[i];
+  }
+  x = x/5;
+  return x;
+}
+
+
+void aktualisiereArr(int * mat, int messwert)
+{
+  for (int i = 0; i < 5; i++)
+  {
+  mat [1] = 1;
+  }
 }
 
 
 void loop()
 {
+  int se1 = analogRead(s1);
+  int se2 = analogRead(s2);
+  int se3 = analogRead(s3);
+  int se4 = analogRead(s4);
+  
+  
+  
+  /*
   drehzahl1 = (analogRead(sensor1)/1023.0)*255.0+100.0;
   drehzahl2 = (analogRead(sensor2)/1023.0)*255.0+100.0;
   if (drehzahl1 > 200 && drehzahl2 < 200)
@@ -45,5 +111,6 @@ void loop()
     Serial.print(drehzahl2);
     Serial.print(";    ");
     lastprint = millis();
-  }
+  }*/
 }
+
